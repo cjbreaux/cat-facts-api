f049eeddbe62c95270656908869ba84731e7fa38
Chris Breaux and kohichi horii <kohichi@>
2019-05-06 15:56:43 -0700

    successfully make api call

---
+++ src/actions/getCatFact.js
@@ -0,0 +1,8 @@
+export default function getCatFact() {
+  return fetch('https://cat-fact.herokuapp.com/facts/random?number=1').then(
+    response => response.json(),
+    error => console.log('An error occured', error)
+  ).then(function(json) {
+    console.log('Result from API:', json)
+  });
+};
--- src/components/CatContainer.jsx
+++ src/components/CatContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import getCatFact from './../actions/getCatFact'
 
 function CatContainer({catFact}) {
   return(
     <div>
       <h1> hello cat </h1>
       <h2>{catFact}</h2>
+      <button onClick={getCatFact}>Get Another Cat Fact</button>
     </div>
   );
 }
